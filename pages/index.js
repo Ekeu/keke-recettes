d90430f5af7662b9932fa7e4e9ca5fbd28d439d2
Ekeu Ulrich <ulrich.ekeu.monkam@efrei.net>
2021-07-23 22:21:21 +0200

    Updated pagination component

--- pages/index.js
+++ pages/index.js
@@ -73,6 +73,7 @@ export default function Home({ recipes, preview }) {
                   Plus de recettes
                 </Button>
               </div>
+              <div className='mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none'></div>
             </div>
           </div>
         </main>
